04addc205d5a28e5d1f12db7a23b4594eb744fba
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-06-18 20:44:10 +0200

    form added

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,29 @@
+import { Field, Form, Formik } from "formik";
+import css from './ContactForm.module.css'
+
 const ContactForm = () => {
-    return (<></>);
+    return (
+        <>
+            <Formik
+                initialValues={{ name: '', number: '', }}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                }}
+            >
+                <Form className={css.container}>
+                    <div className={css.label}>
+                        <p>Name</p>
+                        <Field name='name' className={css.inputField}></Field>
+                    </div>
+                    <div className={css.label}>
+                        <p>Number</p>
+                        <Field name='number' className={css.inputField}></Field>
+                    </div>
+                    <button type='submit' className={css.addContact}>Add Contact</button>
+                </Form>
+            </Formik>
+        </>
+    );
 }
 
 export default ContactForm
