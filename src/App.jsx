b3f3ac4b3ea0d4d28f4c8c3221c9e1a3ab28c285
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-06-18 22:09:45 +0200

    local storage implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import ContactList from './components/ContactList/ContactList'
 import * as Yup from 'yup'
 
 function App() {
-  const [contacts, setContacts] = useState([
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ])
+  const [contacts, setContacts] = useState(() => {
+    const localContacts = localStorage.getItem('contacts');
+    if (localContacts) {
+      return JSON.parse(localContacts);
+    } else {
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+    }
+  });
 
  const [filteredContacts, setFilteredContacts] = useState(contacts);
 
@@ -25,6 +32,7 @@ function App() {
     setFilteredContacts(contacts.filter(contact =>
       contact.name.toLowerCase().includes(value.toLowerCase())
     ));
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts, value])
 
   const handleFormInput = (contact) => {
